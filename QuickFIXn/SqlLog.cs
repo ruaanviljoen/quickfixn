92f9249628066451d6ed9e1d54b8b6b6f876478f
Ruaan Viljoen <ruaanv@gmail.com>
2014-09-09 21:13:13 +0200

    Add SQL log implementation
    Added a simple implementation of a SQL event and message log extending
    the existing ILog interface

---
+++ QuickFIXn/SqlLog.cs
@@ -0,0 +1,210 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace QuickFix
+{
+    public class SqlLog : ILog, System.IDisposable
+    {
+        private readonly object messageLogTableLock = new object();
+        private readonly object eventLogTableLock = new object();
+        private string connectionString;
+
+        private string messageLogTableName;
+        private string eventLogTableName;
+
+        private string prefix;
+
+        private DataTable messageLogTable;
+        private DataTable eventLogTable;
+
+        public string ConnectionString
+        {
+            get { return this.connectionString; }
+        }
+
+        private static DataTable CreateLogTable()
+        {
+            DataTable logTable = new DataTable();
+            logTable.Columns.Add("timestamp", typeof(DateTime));
+            logTable.Columns.Add("message", typeof(string));
+            return logTable;
+        }
+
+        public SqlLog(string connectionString,SessionID sessionID)
+        {
+            this.Init(connectionString, Prefix(sessionID));
+        }
+
+        private void Init(string connectionString,string prefix)
+        {
+            this.connectionString = connectionString;
+
+            this.prefix = prefix;
+            //TODO still use this prefix somewhere - the file log used it in the filename... Use in table names?
+            this.messageLogTableName = "messageslog";
+            this.eventLogTableName = "eventslog";
+
+            lock(messageLogTableLock)
+                this.messageLogTable = CreateLogTable();
+            lock(eventLogTableLock)
+                this.eventLogTable = CreateLogTable();
+        }
+
+        public static string Prefix(SessionID sessionID)
+        {
+            System.Text.StringBuilder prefix = new System.Text.StringBuilder(sessionID.BeginString)
+                .Append('-').Append(sessionID.SenderCompID);
+            if (SessionID.IsSet(sessionID.SenderSubID))
+                prefix.Append('_').Append(sessionID.SenderSubID);
+            if (SessionID.IsSet(sessionID.SenderLocationID))
+                prefix.Append('_').Append(sessionID.SenderLocationID);
+            prefix.Append('-').Append(sessionID.TargetCompID);
+            if (SessionID.IsSet(sessionID.TargetSubID))
+                prefix.Append('_').Append(sessionID.TargetSubID);
+            if (SessionID.IsSet(sessionID.TargetLocationID))
+                prefix.Append('_').Append(sessionID.TargetLocationID);
+
+            if (sessionID.SessionQualifier.Length != 0)
+                prefix.Append('-').Append(sessionID.SessionQualifier);
+
+            return prefix.ToString();
+        }
+
+        public void ClearMessageLog()
+        {
+            lock(messageLogTableLock)
+            {
+                this.messageLogTable.Clear();
+            }
+        }
+
+        public void ClearEventLog()
+        {
+            lock (eventLogTableLock)
+            {
+                this.eventLogTable.Clear();
+            }
+        }
+
+        public void Clear()
+        {
+            this.ClearEventLog();
+            this.ClearMessageLog();
+        }
+        public void OnIncoming(string msg)
+        {
+
+            lock (messageLogTableLock)
+            {
+                DataRow newRow = this.messageLogTable.NewRow();
+                newRow["timestamp"] = DateTime.UtcNow;
+                newRow["message"] = msg;
+                this.messageLogTable.Rows.Add(newRow);
+                this.messageLogTable.AcceptChanges();
+            }
+
+            if (IsMessageLogBatchReady())
+            {
+                lock (messageLogTableLock)
+                {                                        
+                    //SqlConnection dbconn = new SqlConnection(this.connectionString)
+                    BulkCopyToDatabase(this.messageLogTable, this.messageLogTableName);
+                }
+            }
+        }
+
+
+        private virtual bool IsMessageLogBatchReady()
+        {
+            return true;
+        }
+
+        public virtual bool IsEventLogBatchReady()
+        {
+            return true;
+        }
+
+        public void OnOutgoing(string msg)
+        {
+            lock (messageLogTableLock)
+            {
+                DataRow newRow = this.messageLogTable.NewRow();
+                newRow["timestamp"] = DateTime.UtcNow;
+                newRow["message"] = msg;
+                this.messageLogTable.Rows.Add(newRow);
+                this.messageLogTable.AcceptChanges();
+            }
+
+            if (IsMessageLogBatchReady())
+            {
+                lock (messageLogTableLock)
+                {
+                    BulkCopyToDatabase(this.messageLogTable,this.messageLogTableName);
+                }
+            }
+        }
+
+        public virtual void BulkCopyToDatabase(DataTable sourceTable, string targetTableName)
+        {
+            using (SqlConnection dbconn = new SqlConnection(this.connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(
+                "INSERT INTO " + targetTableName + "(timestamp, message) SELECT timestamp, message FROM @MemoryLogTable",
+                dbconn);
+
+                cmd.Parameters.Add(
+                    new SqlParameter()
+                    {
+                        ParameterName = "@MemoryLogTable",
+                        SqlDbType = SqlDbType.Structured,
+                        TypeName = targetTableName,
+                        Value = sourceTable,
+                    });
+
+                cmd.ExecuteNonQuery();
+            }            
+            sourceTable.Clear();
+        }
+
+        public void OnEvent(string s)
+        {
+            lock (eventLogTableLock)
+            {
+                DataRow newRow = this.eventLogTable.NewRow();
+                newRow["timestamp"] = DateTime.UtcNow;
+                newRow["message"] = s;
+                this.eventLogTable.Rows.Add(newRow);
+                this.eventLogTable.AcceptChanges();
+            }
+
+            if (IsEventLogBatchReady())
+            {
+                lock (eventLogTableLock)
+                {
+                    BulkCopyToDatabase(this.eventLogTable, this.eventLogTableName);
+                }
+            }
+        }
+
+        public virtual void Dispose()
+        {
+            //throw new NotImplementedException();
+            //if (this.dbconnection != null)
+            //{
+            //    //attempt to close this cleanly. .Dispose *probably* does so already, but until
+            //    //we research that, close explicitly anyways
+            //    if (this.dbconnection.State == System.Data.ConnectionState.Open)
+            //        this.dbconnection.Close();
+
+            //    this.dbconnection.Dispose();
+            //    this.dbconnection = null;
+            //}
+
+        }
+    }
+}
---
+++ QuickFIXn/SqlLogFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickFix
+{
+    /// <summary>
+    /// Creates a event and messages store that stores messages and events in a sql database
+    /// </summary>
+    public class SqlLogFactory : ILogFactory
+    {
+        SessionSettings settings_;
+
+        #region LogFactory Members
+
+        public SqlLogFactory(SessionSettings settings)
+        {
+            settings_ = settings;
+        }
+
+        /// <summary>
+        /// Creates a sql-based message and event store
+        /// </summary>
+        /// <param name="sessionID">session ID for the message store</param>
+        /// <returns></returns>
+        public ILog Create(SessionID sessionID)
+        {
+
+            return new SqlLog(settings_.Get(sessionID).GetString(SessionSettings.SQL_LOG_CONNECTION_STRING), sessionID);
+        }
+
+        #endregion
+    }
+}
