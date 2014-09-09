0468a087910b7da8cc13439af1365d6145091a4e
Ruaan Viljoen <ruaanv@gmail.com>
2014-09-09 21:23:21 +0200

    Added SQL connectionstring field to SessionSettings

--- QuickFIXn/SessionSettings.cs
+++ QuickFIXn/SessionSettings.cs
@@ -35,6 +35,7 @@ namespace QuickFix
         public const string SOCKET_CONNECT_PORT = "SocketConnectPort";
         public const string RECONNECT_INTERVAL = "ReconnectInterval";
         public const string FILE_LOG_PATH = "FileLogPath";
+        public const string SQL_LOG_CONNECTION_STRING = "SqlLogConnectionString";
         public const string DEBUG_FILE_LOG_PATH = "DebugFileLogPath";
         public const string FILE_STORE_PATH = "FileStorePath";
         public const string REFRESH_ON_LOGON = "RefreshOnLogon";
